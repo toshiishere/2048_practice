87298cb1e167a930e03016c8c3f9b93f4aa4f2e7
TOSHI-TUF <toshi.cs12@nycu.edu.tw>
2025-07-22 00:09:39 +0800

    ready for implementing MCTS

---
+++ codes/MCTS.py
@@ -0,0 +1,5 @@
+from game_engine import Game2048
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+from tqdm import tqdm
--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from 2048-practice!")
-
-
-if __name__ == "__main__":
-    main()
